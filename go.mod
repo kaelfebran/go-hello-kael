01c7917e9ed22d398c67638ca1514cfcb6105333
kaelfebran <michaelfebrans@gmail.com>
2023-08-29 14:16:38 +0700

    update parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kaelfebran/go-hello-kael
+module github.com/kaelfebran/go-hello-kael/v2
 
 go 1.20
--- hello_kael.go
+++ hello_kael.go
@@ -1,5 +1,5 @@
 package go_hello_kael
 
-func HelloKael() string {
-	return "Hello Michael"
+func HelloKael(name string) string {
+	return "Hello" + name
 }
